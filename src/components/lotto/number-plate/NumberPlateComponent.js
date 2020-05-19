836d4980fed8deb3f235f1b6abe322d1f9ae81dd
alvinkim01 <55009265+alvinkim01@users.noreply.github.com>
2020-05-19 10:44:36 +0900

    1

---
+++ src/components/lotto/number-plate/NumberPlateComponent.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import "./NumberPlateComponent.css";
+import NumberPlateBoxComponent from './NumberPlateBoxComponent';
+
+class NumberPlateComponent extends Component {
+
+    render(){
+        return(
+            <div>
+                <div className="number-plate-contents">
+                { this.props.selected.map((select, index) =>
+                    <NumberPlateBoxComponent
+                    key={index}
+                    number={index+1}
+                    selected={select}
+                    handleChange={this.props.handleChange}
+                    
+                    />
+                )}
+                </div>
+                <button onClick={this.props.handleButtonOnClick} className="number-button">번호 생성</button>
+            </div>
+        );
+    }
+}
+export default NumberPlateComponent;
